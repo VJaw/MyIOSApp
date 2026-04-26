d2563cc0c129d6e2c3f04360934af7dbf1b0e7b3
Vijaya Jawadi <vijayajawadi@Vijayas-MacBook-Pro.local>
2026-04-26 16:11:18 -0700

    my first project

---
+++ MyIOSApp/MyIOSApp/MyIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIOSAppApp.swift
+//  MyIOSApp
+//
+//  Created by Vijaya Jawadi on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct MyIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyIOSApp/MyIOSAppTests/MyIOSAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  MyIOSAppTests.swift
+//  MyIOSAppTests
+//
+//  Created by Vijaya Jawadi on 4/26/26.
+//
+
+import Testing
+@testable import MyIOSApp
+
+struct MyIOSAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
